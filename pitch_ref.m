dc37a7ae62a62e3195e9a4409378dd040bb8711d
GentaSasaki <s615g615@icloud.com>
2022-09-07 00:13:35 +0900

    noprogress

--- pitch_ref.m
+++ pitch_ref.m
@@ -3,8 +3,15 @@
 sampleTime  = 0.01;
 numSteps = 1001;
 time = sampleTime*(0:numSteps-1);
-data = sin(2*pi/3*time);
-
-inputdata = timeseries(data,time);
-save("sin_wave","inputdata","-v7.3");
-
+data = 0.0;
+d = 0.5;
+%inputdata = timeseries(data,time);
+%inputdata = timeseries(d,data);
+p_ref = load("data.csv");
+A_p = [0,1;0,0];
+B_p = [0,0;0,21.6319];
+C_p = [1,0;0,1];
+D_p = [0,0;0,0];
+%save("p_ref","inputdata","-v7.3");%.matファイルで保存
+save("p_ref","p_ref","-v7.3");
+%plot(data,time);
